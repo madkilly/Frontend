e9ac50b5d5c1e7b798b03fc881a3ad6f5f2fb909
zhukai1 <zhukai1@yonyou.com>
2016-07-27 16:15:41 +0800

    koa脚手架搭建

--- gulpTest/server/mockserver.js
+++ gulpTest/server/mockserver.js
@@ -5,6 +5,7 @@ var serve = require('koa-static');//koa框架维护静态资源
 var mount = require('koa-mount');//将koa应用作为中间件挂载（实现url不同的context拦截）
 var koa = require('koa');//koa框架引入
 var path = require('path');//用来拼接路径
+var Router = require('koa-router');
 
 module.exports = function(options){
     var app = options.app;
@@ -14,11 +15,17 @@ module.exports = function(options){
     var application=koa();
     return {
         start: function() {
-            application.use(serve(path.join(__dirname, '../src')));//（../src是因为该js文件处于次级目录要回到根目录定位src目录）
+            var router = new Router({
+                prefix: context
+            });
+           application.use(serve(path.join(__dirname, '../src')));//（../src是因为该js文件处于次级目录要回到根目录定位src目录）
             console.log(path.join(__dirname, '../src'));
             app.use(mount(context,application));//挂载koa application在指定的url上
+           // app.use(serve(path.join(__dirname, '../src')));
+/*            app.use(router.routes())
+                .use(router.allowedMethods());*/
             app.listen(port);
-            console.log('web服务8088已启动');
+            console.log('web服务'+port+'已启动');
         }
     }
 }
