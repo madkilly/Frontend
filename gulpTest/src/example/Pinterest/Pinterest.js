ea5506e48712600f5aa5be012fb30b40c086974f
zhukai1 <zhukai1@yonyou.com>
2016-09-30 16:57:49 +0800

    瀑布流

--- gulpTest/src/example/Pinterest/Pinterest.js
+++ gulpTest/src/example/Pinterest/Pinterest.js
@@ -1,10 +1,6 @@
 $(document).ready(function() {
     $('#pinBoot').pinterest_grid({
-        no_columns: 4,
-        padding_x: 10,
-        padding_y: 10,
-        margin_bottom: 50,
-        single_column_breakpoint: 700
+        no_columns: 4
     });
 });
 
---
+++ gulpTest/src/example/Pinterest/pinterest.md
@@ -0,0 +1,8 @@
+box-shadow: 0 10px 30px rgba(0,0,0,.3)
+css 阴影边框效果
+
+setTimeout() 方法用于在指定的毫秒数后调用函数或计算表达式.
+clearTimeout() 方法可取消由 setTimeout() 方法设置的 timeout.
+
+$.fn[pluginName] = function (options) {}
+为jquery实例添加函数
